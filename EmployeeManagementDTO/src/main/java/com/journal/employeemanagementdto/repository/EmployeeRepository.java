2b884b2eabb103e07473c2321cffe9aec800148a
Arshul26 <arsulamin0@gmail.com>
2025-12-19 10:45:15 +0530

    Switched from in memory database to real server database

--- EmployeeManagementDTO/src/main/java/com/journal/employeemanagementdto/repository/EmployeeRepository.java
+++ EmployeeManagementDTO/src/main/java/com/journal/employeemanagementdto/repository/EmployeeRepository.java
@@ -5,3 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }
+//Employee is the entity class the repository manages,
+//and Long is the data type of its primary key.
+//Spring Data JPA uses this information to auto-generate CRUD methods.
